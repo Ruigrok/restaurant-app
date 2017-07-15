be49f9ab034775140de04c1de864ae2e844bcd16
AC Ruigrok <andrew.c.ruigrok@gmail.com>
2017-07-15 13:19:59 -0500

    made html consistent across pages

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,15 +1,26 @@
 var tableData = require('../data/table-data.js');
 var waitlistData = require('../data/waitlist-data.js');
 
-module.exports = function(app) {
-  
-  app.get('/api/tables', function(req, res) {
+module.exports = function (app) {
+
+  app.get('/api/tables', function (req, res) {
     res.json(tableData);
   });
 
-  app.get('/api/waitlist', function(req, res) {
+  app.get('/api/waitlist', function (req, res) {
     res.json(waitlistData);
-  })
+  });
+
+  app.post('/api/tables', function (req, res) {
+
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    } else {
+      waitlistData.push(req.body);
+      res.json(false);
+    }
+  });
 }
 
 
